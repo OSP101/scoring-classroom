1a32e3182e5cb36fdb15b1ae0314c1f78689d02e
OSP101 <supphitan.pk@gmail.com>
2025-07-13 03:34:17 +0700

    Turnstile config

--- pages/myscore/index.tsx
+++ pages/myscore/index.tsx
@@ -464,7 +464,7 @@ export default function index() {
                                     className="bg-gradient-to-r from-purple-500 to-blue-500 text-white font-semibold px-4 sm:px-6 md:px-8 py-2 sm:py-3 md:py-4 rounded-xl shadow-lg hover:shadow-xl transition-all duration-300 text-sm sm:text-base md:text-md transform hover:scale-105 w-full md:w-auto" 
                                     onClick={submutations}
                                     size="lg"
-                                    disabled={statusUpdate}
+                                    isDisabled={statusUpdate || pointInput.length === 0 || !canSubmit}
                                     startContent={statusUpdate ? <Spinner color="white" size="sm" /> : <span className="text-lg sm:text-xl">🔍</span>}
                                 >
                                     {statusUpdate ? "กำลังค้นหา..." : "ค้นหาคะแนน"}
